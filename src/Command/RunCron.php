ed5b6dee4508bffc90fdd50736b9ffcc36eaef67
Ivan Kayzer <ivan.kayzer@outlook.com>
2021-03-12 18:33:04 +0100

    wip

--- src/Command/RunCron.php
+++ src/Command/RunCron.php
@@ -8,6 +8,8 @@ use App\Entity\TaskExecutionHistory;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
+use Nesk\Puphpeteer\Puppeteer;
+use Nesk\Rialto\Data\JsFunction;
 use React\EventLoop\Factory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -76,27 +78,48 @@ class RunCron extends Command
 
                 foreach ($scripts as $script) {
                     echo "EXECUTING SCRIPT: " . $script->getId() . PHP_EOL;
+                    /** @var ScriptOutput|null $previousOutput */
+                    $previousOutput = $this->entityManager
+                        ->getRepository(ScriptOutput::class)
+                        ->findPrevious($script);
+
+                    $output = new ScriptOutput();
+                    $output->setExecutionHistory($executionHistory);
+                    $output->setScript($script);
+
                     if ($script->getType() === 'snapshot') {
-                        /** @var ScriptOutput|null $previousOutput */
-                        $previousOutput = $this->entityManager
-                            ->getRepository(ScriptOutput::class)
-                            ->findPrevious($script);
-
-                        $output = new ScriptOutput();
-                        $output->setExecutionHistory($executionHistory);
-                        $output->setScript($script);
-
                         $response = $this->guzzle->get($task->getUrl());
                         $newOutput = $response->getBody()->getContents();
+                    } elseif ($script->getType() === 'execute') {
+                        $puppeteer = new Puppeteer;
 
-                        if (!$previousOutput || $previousOutput->getOutput() !== $newOutput) {
-                            echo "FOUND CHANGES. NOTIFYING" . PHP_EOL;
-                            $output->setOutput($newOutput);
-                            $this->bot->sendMessage($task->getUser()->getProviderId(), 'Changed: ' . $task->getName());
-                        }
+                        $browser = $puppeteer->launch([
+                            'args' => [
+                                '--no-sandbox',
+                                '--disable-setuid-sandbox',
+                                '--disable-dev-shm-usage',
+                            ],
+                        ]);
+                        $page = $browser->newPage();
+                        $page->setViewport(['width' => 1366, 'height' => 768]);
+                        $page->setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36');
 
-                        $this->entityManager->persist($output);
+                        $page->addScriptTag(['url' => 'https://code.jquery.com/jquery-3.5.1.min.js']);
+
+                        $page->goto($task->getUrl());
+                        $newOutput = $page->evaluate(JsFunction::createWithBody("
+                            return document.innerHtml;
+                        "));
+                        $browser->close();
                     }
+
+                    if (!$previousOutput || $previousOutput->getOutput() !== $newOutput) {
+                        echo "FOUND CHANGES. NOTIFYING" . PHP_EOL;
+                        $output->setOutput($newOutput);
+                        $this->bot->sendMessage($task->getUser()->getProviderId(), 'Changed: ' . $task->getName());
+                    }
+
+                    $this->entityManager->persist($output);
                 }
 
                 $executionHistory->finish();
